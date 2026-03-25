f88c53225e346e3d0bd420d1d4f7fe07514802ba
pa1nwizzard <meeponekrisa@gmail.com>
2026-03-25 18:43:44 +0500

    added hourly forecast

---
+++ lib/data/weekdays.dart
@@ -0,0 +1,9 @@
+Map<int, String> weekdays = {
+  1 : 'Mon.',
+  2 : 'Tue.',
+  3 : 'Wed.',
+  4 : 'Thu.',
+  5 : 'Fri.',
+  6 : 'Sat.',
+  7 : 'Sun.'
+};
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/data/weather.dart';
+import 'package:weather_app/data/weekdays.dart';
 import 'package:weather_app/services/api_service.dart';
 import 'package:weather_app/widgets/styles.dart';
 
@@ -65,8 +64,8 @@ class _HomeState extends State<Home> {
                   padding: const EdgeInsets.only(top: 160),
                   child: Center(
                     child: Text(
-                      weather.degreesCelsius.isEmpty ? '' : 'Макс.: ${sortedWeather[0][23].round()}°, мин.: ${sortedWeather[0][0].round()}°',
-                      style: GoogleFonts.josefinSans(textStyle: textWhiteStyle, fontSize: 26),
+                      weather.degreesCelsius.isEmpty ? '' : 'Max.: ${sortedWeather[0][23].round()}°, min.: ${sortedWeather[0][0].round()}°',
+                      style: GoogleFonts.josefinSans(textStyle: textWhiteStyle, fontSize: 28),
                     ),
                   ),
                 ),
@@ -78,14 +77,109 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.only(top: 32, left: 16, right: 16),
             child: Container(
               height: 120,
-              decoration: boxWhiteDecoration
+              decoration: boxWhiteDecoration,
+              child: Scaffold(
+                backgroundColor: Color.fromARGB(0, 0, 0, 0),
+                appBar: AppBar(
+                  title: Column(
+                    spacing: 2,
+                    children: [
+                      Text(
+                        'Hourly forecast',
+                        style: GoogleFonts.josefinSans(
+                          fontSize: 24,
+                          textStyle: TextStyle(color: Colors.white)
+                        ),
+                      ),
+                      Container(
+                        width: 1000,
+                        height: 1.5,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  centerTitle: true,
+                  backgroundColor: Color.fromARGB(0, 0, 0, 0),
+                ),
+                body: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 24,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 12, right: 8),
+                      child: Column(
+                        children: [
+                          Text(
+                            '$index',
+                            style: GoogleFonts.josefinSans(
+                              fontSize: 16,
+                              textStyle: TextStyle(color: Colors.white)
+                            )
+                          ),
+                          Text(
+                            weather.degreesCelsius.isEmpty ? '' : '${weather.degreesCelsius[0][index].round()}°',
+                            style: GoogleFonts.josefinSans(
+                              fontSize: 16,
+                              textStyle: TextStyle(color: Color.fromARGB(255, 0, 127, 173))
+                            )
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              )
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 0),
+            padding: const EdgeInsets.only(top: 8, left: 16, right: 16, bottom: 0),
             child: Container(
-              height: 800,
-              decoration: boxWhiteDecoration
+              height: 510,
+              decoration: boxWhiteDecoration,
+              child: Scaffold(
+                backgroundColor: Color.fromARGB(0, 0, 0, 0),
+                appBar: AppBar(
+                  title: Column(
+                    spacing: 2,
+                    children: [
+                      Text(
+                        'Forecast for 10 days',
+                        style: GoogleFonts.josefinSans(
+                          fontSize: 24,
+                          textStyle: TextStyle(color: Colors.white)
+                        ),
+                      ),
+                      Container(
+                        width: 1000,
+                        height: 1.5,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  centerTitle: true,
+                  backgroundColor: Color.fromARGB(0, 0, 0, 0),
+                ),
+                body: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsetsGeometry.all(8),
+                      child: Row(
+                        children: [
+                          Text(
+                            index == 0 ? 'Today' : '${dt.weekday + index <= 7 ? weekdays[dt.weekday + index] : weekdays[dt.weekday + index - 7]}',
+                            style: GoogleFonts.josefinSans(
+                              fontSize: 20,
+                              textStyle: TextStyle(color: Colors.white)
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
           ),
         ],
